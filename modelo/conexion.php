dbcf3af5d52e3b7ad72a52f1bb0670379a723d71
Giselle276 <gisellemaccha27@gmail.com>
2024-09-26 10:23:47 +0200

    modificando desde rama2

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -8,7 +8,7 @@ class conexion{
     private $contrasena = "";
     public $pdo = null;
     //fuerza los nombres de la columna al momento de retornar los convierte en minusculas
-    //Reporte de errores para poder lanzar excepciones mediante try catch
+    
     private $atributos=[PDO::ATTR_CASE =>PDO::CASE_LOWER,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING,PDO::ATTR_DEFAULT_FETCH_MODE=> PDO::FETCH_OBJ];
     function __construct(){
         $this->pdo = new PDO("mysql:dbname={$this->db};host={$this ->servidor};port={$this->puerto};charset={$this->charset}",$this->usuario,$this->contrasena,$this->atributos);
