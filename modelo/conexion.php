6131231e4d0e00d06f1f3c267690c8164a09b7b8
Giselle276 <gisellemaccha27@gmail.com>
2024-09-26 10:20:34 +0200

    modificando desde rama2

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -9,8 +9,6 @@ class conexion{
     public $pdo = null;
     //fuerza los nombres de la columna al momento de retornar los convierte en minusculas
     //Reporte de errores para poder lanzar excepciones mediante try catch
-    //que las cadenas vacias retornadas son convertidas a null
-    //Modo de busqueda que nos retorna objetos actualizados
     private $atributos=[PDO::ATTR_CASE =>PDO::CASE_LOWER,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING,PDO::ATTR_DEFAULT_FETCH_MODE=> PDO::FETCH_OBJ];
     function __construct(){
         $this->pdo = new PDO("mysql:dbname={$this->db};host={$this ->servidor};port={$this->puerto};charset={$this->charset}",$this->usuario,$this->contrasena,$this->atributos);
